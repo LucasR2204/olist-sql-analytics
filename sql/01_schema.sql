5c4a324941e4145c52c938ecf6e5949625c26725
Lucas2204 <104332690+Lucaas2204@users.noreply.github.com>
2026-07-01 14:28:13 +0200

    Add products and category translation tables

--- sql/01_schema.sql
+++ sql/01_schema.sql
@@ -17,3 +17,16 @@ CREATE TABLE product_category_translation(
     product_category_name TEXT PRIMARY KEY,
     product_category_name_english TEXT NOT NULL
 );
+
+CREATE TABLE products(
+    product_id TEXT PRIMARY KEY,
+    product_category_name TEXT NOT NULL,
+    product_name_lenght INT NOT NULL,
+    product_description_lenght INT NOT NULL,
+    product_photos_qty INT NOT NULL,
+    product_weight_g FLOAT NOT NULL,
+    product_length_cm FLOAT NOT NULL,
+    product_height_cm FLOAT NOT NULL,
+    product_width_cm FLOAT NOT NULL,
+    FOREIGN KEY (product_category_name) REFERENCES product_category_translation (product_category_name)
+);
