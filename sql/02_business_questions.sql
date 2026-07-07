51f21112f4c8658f5d208898bb4234145df1b1d5
Lucas2204 <104332690+Lucaas2204@users.noreply.github.com>
2026-07-07 15:22:41 +0200

    Add CTE and window function query: seller revenue rank by state

--- sql/02_business_questions.sql
+++ sql/02_business_questions.sql
@@ -1,4 +1,6 @@
-SELECT product_category_translation.product_category_name_english, SUM(price) AS total_revenue
+SELECT 
+    product_category_translation.product_category_name_english,
+    SUM(price) AS total_revenue
 FROM order_items
 
 JOIN products ON order_items.product_id = products.product_id
@@ -7,3 +9,23 @@ JOIN product_category_translation ON products.product_category_name = product_ca
 GROUP BY product_category_translation.product_category_name
 ORDER BY total_revenue DESC
 LIMIT 10;
+
+WITH cte_state_price_rank AS (
+    
+    SELECT 
+        sellers.seller_state,
+        sellers.seller_id,
+        SUM(price) as total_revenue
+    from sellers
+
+    JOIN order_items ON sellers.seller_id = order_items.seller_id
+
+    GROUP BY sellers.seller_id , sellers.seller_state
+
+
+)
+SELECT
+    *
+    RANK() OVER (PARTITION BY seller_state,seller_id,total_revenue ORDER BY total_revenue DESC) AS revenue_rank
+FROM cte_state_price_rank
+ORDER BY revenue_rank, seller_state
